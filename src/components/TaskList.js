f05989efc7d2b3416071035678f2f177ea719b00
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-04-24 17:24:57 +0300

    Imported TASKS data to be displayed in tasklist component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { TASKS } from "../data";
+import Task from "./Task";
+
 
 function TaskList() {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+     {TASKS.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
     </div>
   );
 }
